0287d97ee9509d00de8b28c8057085c12f5d0767
Chris <cp2533@gmail.com>
2023-01-18 11:15:41 -0600

    added Todo components

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { useSelector} from 'react-redux';
 
 
+
+function Todo(props)
+{
+  return(
+    <div><input type={"checkbox"} checked={props.complete}></input> {props.description}</div>
+  )
+}
 
 function TodoList(props)
 {
-    return (<div>TodoList</div>);
+  const state = useSelector((state)=>state);
+  return (
+    <>
+      {state.map(item => (
+      <>
+        <Todo description={item.description} complete={item.complete}/>
+      </>
+      ))}
+    </>
+  );
 }
 
 export default function Todos() {
