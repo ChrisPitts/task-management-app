e1dd2e942b0711580a2a039e7b6d324f7c588999
Chris Pitts <cp25393@gmail.com>
2023-01-21 12:32:15 -0600

    added names to form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,13 +4,13 @@ export default function Contact() {
   return (
     <form>
         <label htmlFor='name'>Name</label><br/>
-        <input type="text" id='name'/><br/>
+        <input type="text" name='name' id='name'/><br/>
 
         <label htmlFor='email'>Email</label><br/>
-        <input type="email" id='email'/><br/>
+        <input type="email" name='email' id='email'/><br/>
 
         <label htmlFor='message'>Message</label><br/>
-        <textarea id='message' rows={10}></textarea><br/>
+        <textarea id='message' name='message' rows={10}></textarea><br/>
 
         <button type="submit">Send</button>
     </form>
