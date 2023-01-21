e65fd1251be3a847b57a2f890f37f21924208d16
Chris Pitts <cp25393@gmail.com>
2023-01-21 12:29:28 -0600

    added basic form layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <form>
+        <label htmlFor='name'>Name</label><br/>
+        <input type="text" id='name'/><br/>
+
+        <label htmlFor='email'>Email</label><br/>
+        <input type="email" id='email'/><br/>
+
+        <label htmlFor='message'>Message</label><br/>
+        <textarea id='message' rows={10}></textarea><br/>
+
+        <button type="submit">Send</button>
+    </form>
   )
 }
