23b44090eb5009449dd82fc6acef1887044a31fc
Chris Pitts <cp25393@gmail.com>
2023-01-21 16:33:14 -0600

    Made contact form track its own state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Contact() {
-  return (
-    <form>
+export default class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
+  }
+  handleNameChange(event) {
+    this.setState({ name: event.target.value});
+  }
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value});
+  }
+  handleMessageChange(event) {
+    this.setState({ message: event.target.value});
+  }
+  render() {
+    console.dir(this.state);
+    return (
+      <form>
         <label htmlFor='name'>Name</label><br/>
-        <input type="text" name='name' id='name'/><br/>
+        <input type="text" name='name' id='name' value={this.state.name} onChange={this.handleNameChange.bind(this)} required/><br/>
 
         <label htmlFor='email'>Email</label><br/>
-        <input type="email" name='email' id='email'/><br/>
+        <input type="email" name='email' id='email' value={this.state.email} onChange={this.handleEmailChange.bind(this)} required/><br/>
 
         <label htmlFor='message'>Message</label><br/>
-        <textarea id='message' name='message' rows={10}></textarea><br/>
+        <textarea id='message' name='message' rows={10} value={this.state.message} onChange={this.handleMessageChange.bind(this)} required></textarea><br/>
 
         <button type="submit">Send</button>
     </form>
-  )
+    )
+  }
 }
