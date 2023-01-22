0ba20f5b4d9c223f54007bf135c40cfbfa32caa0
Chris Pitts <cp25393@gmail.com>
2023-01-21 18:56:44 -0600

    added last name input

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,13 +4,17 @@ export default class Contact extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
+      firstName: '',
+      lastName: '',
       email: '',
       message: ''
     }
   }
-  handleNameChange(event) {
-    this.setState({ name: event.target.value});
+  handleFirstNameChange(event) {
+    this.setState({ firstName: event.target.value});
+  }
+  handleLastNameChange(event) {
+    this.setState({ lastName: event.target.value});
   }
   handleEmailChange(event) {
     this.setState({ email: event.target.value});
@@ -19,12 +23,12 @@ export default class Contact extends Component {
     this.setState({ message: event.target.value});
   }
   render() {
-    console.dir(this.state);
     return (
       <form>
-        <label htmlFor='name'>Name</label><br/>
-        <input type="text" name='name' id='name' value={this.state.name} onChange={this.handleNameChange.bind(this)} required/><br/>
-
+        <label htmlFor='first-name'>First Name</label><br/>
+        <input type="text" name='first-name' id='first-name' value={this.state.firstName} onChange={this.handleFirstNameChange.bind(this)} required/><br/>
+        <label htmlFor='last-name'>Last Name</label><br/>
+        <input type="text" name='last-name' id='last-name' value={this.state.lastName} onChange={this.handleLastNameChange.bind(this)} required/><br/>
         <label htmlFor='email'>Email</label><br/>
         <input type="email" name='email' id='email' value={this.state.email} onChange={this.handleEmailChange.bind(this)} required/><br/>
 
